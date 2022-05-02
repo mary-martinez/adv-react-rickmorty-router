d42349896381328ac7cc9d2d66bb396d1569d7df
Mary Martinez <mary.diana.martinez@gmail.com>
2022-05-02 16:44:16 -0700

    passing initial test for character data

--- src/App.test.jsx
+++ src/App.test.jsx
@@ -2,10 +2,11 @@ import { render, screen } from '@testing-library/react';
 import App from './App';
 
 describe('App', () => {
-  it('should navigate between a list view and a detail view', () => {
+  it('should navigate between a list view and a detail view', async () => {
     render(<App />)
     screen.getByText(/loading/i);
-    
+
+    await screen.findByText('Morty Smith');
 
   })
 })
